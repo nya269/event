67340eb90694731961ed2ad03ec08c2f3b14491e
Desire077 <ngudesijaph@gmail.com>
2025-12-07 18:01:13 +0100

    Revert "j'ai fais les modifs pour vous bg"

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,111 +1,17 @@
-import sequelize from '../config/db.js';
-import User from './User.js';
-import Event from './Event.js';
-import Inscription from './Inscription.js';
-import Payment from './Payment.js';
-import RefreshToken from './RefreshToken.js';
+const { users} = require ('.schemas/userSchema');
+const { events } = require  ('./schemas/eventSchema');
+const { inscriptions } = require ('./schemas/inscriptionSchema');
+const { payements } = require ('./schemas/paymentSchema');
 
-// Define associations
-
-// User - Event (One-to-Many: User can organize many events)
-User.hasMany(Event, {
-  foreignKey: 'organizer_id',
-  as: 'organizedEvents',
-});
-Event.belongsTo(User, {
-  foreignKey: 'organizer_id',
-  as: 'organizer',
-});
-
-// User - Inscription (One-to-Many: User can have many inscriptions)
-User.hasMany(Inscription, {
-  foreignKey: 'user_id',
-  as: 'inscriptions',
-});
-Inscription.belongsTo(User, {
-  foreignKey: 'user_id',
-  as: 'user',
-});
-
-// Event - Inscription (One-to-Many: Event can have many inscriptions)
-Event.hasMany(Inscription, {
-  foreignKey: 'event_id',
-  as: 'inscriptions',
-});
-Inscription.belongsTo(Event, {
-  foreignKey: 'event_id',
-  as: 'event',
-});
-
-// User - Payment (One-to-Many: User can have many payments)
-User.hasMany(Payment, {
-  foreignKey: 'user_id',
-  as: 'payments',
-});
-Payment.belongsTo(User, {
-  foreignKey: 'user_id',
-  as: 'user',
-});
-
-// Event - Payment (One-to-Many: Event can have many payments)
-Event.hasMany(Payment, {
-  foreignKey: 'event_id',
-  as: 'payments',
-});
-Payment.belongsTo(Event, {
-  foreignKey: 'event_id',
-  as: 'event',
-});
-
-// Inscription - Payment (One-to-One: Inscription can have one payment)
-Inscription.hasOne(Payment, {
-  foreignKey: 'inscription_id',
-  as: 'payment',
-});
-Payment.belongsTo(Inscription, {
-  foreignKey: 'inscription_id',
-  as: 'inscription',
-});
-
-// User - RefreshToken (One-to-Many: User can have many refresh tokens)
-User.hasMany(RefreshToken, {
-  foreignKey: 'user_id',
-  as: 'refreshTokens',
-  onDelete: 'CASCADE',
-});
-RefreshToken.belongsTo(User, {
-  foreignKey: 'user_id',
-  as: 'user',
-});
-
-// Many-to-Many: User participates in Events through Inscriptions
-User.belongsToMany(Event, {
-  through: Inscription,
-  foreignKey: 'user_id',
-  otherKey: 'event_id',
-  as: 'participatingEvents',
-});
-Event.belongsToMany(User, {
-  through: Inscription,
-  foreignKey: 'event_id',
-  otherKey: 'user_id',
-  as: 'participants',
-});
-
-export {
-  sequelize,
-  User,
-  Event,
-  Inscription,
-  Payment,
-  RefreshToken,
+module.exports = {
+    users,
+    events,
+    inscriptions,
+    payements,
 };
 
-export default {
-  sequelize,
-  User,
-  Event,
-  Inscription,
-  Payment,
-  RefreshToken,
-};
+// Maintenant, dans d'autres fichiers, on peut faire :
+// const { users, events } = require('../models');
+// Au lieu de :
+// const { users } = require('../models/schemas/userSchema');
+// const { events } = require('../models/schemas/eventSchema');
